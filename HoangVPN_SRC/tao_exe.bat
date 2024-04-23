b61c80851a3f450d01091a6fbbc9d5586385f67a
Hoang Nguyen <96934829+lordnic99@users.noreply.github.com>
2024-04-23 11:12:51 +0700

    Update tao_exe.bat

--- HoangVPN_SRC/tao_exe.bat
+++ HoangVPN_SRC/tao_exe.bat
@@ -1,9 +1,9 @@
 
 @echo off
 
-REM pyinstaller .\run.py -w --noconsole --noconfirm -n HoangVPN --icon=ico.ico --add-data "rathole.exe:." --add-data "wiresock.msi:." --add-REM data "wireguard.msi:." --add-data "nssm.exe:." --add-data "rathole_client.exe:." --add-data "api.json:." --upx-dir UPX
-
-python.exe -m nuitka --mingw64 .\run.py --standalone --onefile --remove-output --output-filename="HoangVPN"  --windows-icon-from-ico=ico.ico --windows-uac-admin  --windows-disable-console --include-data-file=resources/*=./ --plugin-enable=upx --upx-binary=UPX
+python.exe -m nuitka --mingw64 .\run.py --standalone --onefile --remove-output --output-filename="HoangVPN"  --windows-icon-from-ico=ico.ico --windows-uac-admin  --windows-disable-console --include-data-file=resources/*=./
+UPX\upx.exe --best HoangVPN.exe
 
 echo Tao file exe thanh cong, kiem tra folder dist de su dung!
+
 pause
